b296a40eec4e3eb75bf7bf35cfbc584eb6c41cbd
KeanuRayhan <117803802+KeanuRayhan@users.noreply.github.com>
2023-02-18 22:33:54 +0700

    Add files via upload

---
+++ TestCylinder.java
@@ -0,0 +1,49 @@
+
+public class TestCylinder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Declare and allocate a new instance of cylinder
+		 // with default color, radius, and height
+		 Cylinder c1 = new Cylinder();
+		 System.out.println(c1.toString());
+		 System.out.println("Cylinder:"
+		 + " radius=" + c1.getRadius()
+		 + " height=" + c1.getHeight()
+		 + " base area=" + c1.getArea()
+		 + " volume=" + c1.getVolume() + "\n");
+
+		 // Declare and allocate a new instance of cylinder
+		 // specifying height, with default color and radius
+		 Cylinder c2 = new Cylinder(10.0);
+		 System.out.println(c2.toString());
+		 System.out.println("Cylinder:"
+		 + " radius=" + c2.getRadius()
+		 + " height=" + c2.getHeight()
+		 + " base area=" + c2.getArea()
+		 + " volume=" + c2.getVolume() + "\n");
+
+		 // Declare and allocate a new instance of cylinder
+		 // specifying radius and height, with default color
+		 Cylinder c3 = new Cylinder(2.0, 10.0);
+		 System.out.println(c3.toString());
+		 System.out.println("Cylinder:"
+		 + " radius=" + c3.getRadius()
+		 + " height=" + c3.getHeight()
+		 + " base area=" + c3.getArea()
+		 + " volume=" + c3.getVolume() + "\n");
+		 
+		 // Declare and allocate a new instance of cylinder
+		 // specifying radius and height, and color
+		 Cylinder c4 = new Cylinder("blue", 2.0, 10.0);
+		 System.out.println(c4.toString());
+		 System.out.println("Cylinder:"
+		 + " radius=" + c4.getRadius()
+		 + " height=" + c4.getHeight()
+		 + " base area=" + c4.getArea()
+		 + " volume=" + c4.getVolume() + "\n");
+		 
+	}
+
+}
